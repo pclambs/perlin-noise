c3a72682bb7d5a0b51754f1f787d2c52657eae25
deepwubs <pchucklambs@gmail.com>
2023-11-03 13:52:00 -0500

    in a working state of 1d terrain generation

--- src/components/PerlinNoise/index.jsx
+++ src/components/PerlinNoise/index.jsx
@@ -6,8 +6,10 @@ const PerlinNoise = () => {
 const canvasRef = useRef()
 
   const Sketch = p5 => {
-    let xoff1 = 0
-    let xoff2 = 10000
+    // let xoff1 = 0
+    // let xoff2 = 10000
+    let inc = 0.01
+    let start = 0
 
     p5.setup = () => {
       p5.createCanvas(p5.windowWidth, p5.windowHeight)
@@ -15,13 +17,30 @@ const canvasRef = useRef()
 
     p5.draw = () => {
       p5.background(51)
-      let x = p5.map(p5.noise(xoff1), 0, 1, 0, p5.width)
-      let y = p5.map(p5.noise(xoff2), 0, 1, 0, p5.height)
+      p5.stroke(255)
+      p5.noFill()
+      p5.beginShape()
+      let xoff = start
+      for (let x = 0; x < p5.width; x++) {
+        p5.stroke(255)
+        let y = p5.noise(xoff) * p5.height
+        p5.vertex(x, y)
 
-      xoff1 += 0.01
-      xoff2 += 0.01
+        xoff += inc
+      }
+      p5.endShape()
 
-      p5.ellipse(x, y, 24, 24)
+      start += inc
+
+      // p5.noLoop()
+
+      // let x = p5.map(p5.noise(xoff1), 0, 1, 0, p5.width)
+      // let y = p5.map(p5.noise(xoff2), 0, 1, 0, p5.height)
+
+      // xoff1 += 0.01
+      // xoff2 += 0.01
+
+      // p5.ellipse(x, y, 24, 24)
     }
 
     p5.windowResized = () => {
