611685ece9200e124a5b0d9b62f7cfefc8e2ed87
deepwubs <pchucklambs@gmail.com>
2023-11-04 15:20:21 -0500

    adding color and changing hue based on history

--- src/components/PerlinNoise/index.jsx
+++ src/components/PerlinNoise/index.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useRef } from 'react'
 import p5 from 'p5'
 import './PerlinNoise.css'
 
-
 const PerlinNoise = () => {
   const canvasRef = useRef()
   const Sketch = p5 => {
-    const inc = .2
+    const inc = .1
     const scl = 10
     let cols, rows
     let zoff = 0
@@ -18,8 +17,8 @@ const PerlinNoise = () => {
         this.pos = p5.createVector(p5.random(p5.width), p5.random(p5.height))
         this.vel = p5.createVector(0,0)
         this.acc = p5.createVector(0,0)
-        this.maxspeed = 4
-        this.historySize = 5
+        this.maxspeed = 2
+        this.historySize = 15
         this.history = new Array(this.historySize)
         this.historyIndex = 0
       }
@@ -35,6 +34,8 @@ const PerlinNoise = () => {
       follow(vectors) {
         let x = p5.floor(this.pos.x / scl)
         let y = p5.floor(this.pos.y / scl)
+        x = p5.constrain(x, 0, cols - 1)
+        y = p5.constrain(y, 0, rows - 1)
         let index = x + y * cols
         let force = vectors[index]
         this.applyForce(force)
@@ -64,8 +65,12 @@ const PerlinNoise = () => {
         }
       
         if (wrapped) {
-          this.history = new Array(this.historySize)
-          this.historyIndex = 0
+          for (let i = 0; i < this.historySize; i++) {
+            if (this.history[i] !== undefined) {
+              this.history[i].x = this.pos.x
+              this.history[i].y = this.pos.y
+            }
+          }
         }
       }
 
@@ -74,16 +79,33 @@ const PerlinNoise = () => {
         this.historyIndex = (this.historyIndex + 1) % this.historySize
       }
       
-      
       show() {
-        p5.stroke(255, 5)
-        this.p5.strokeWeight(1)
+        // let hue = p5.map(this.pos.x, 0, p5.width, 0, 255)
+
+        let maxOpacity = 100
+        let minOpacity = 5
+
         this.p5.noFill()  
-        p5.beginShape()
-        for (let i = this.historyIndex, count = 0; count < this.historySize; i = (i + 1) % this.historySize, count++) {
-          let pos = this.history[i]
+        for (let i = this.historySize - 1; i >= 1; i--) {
+          let index = (this.historyIndex -1) % this.historySize
+          let pos = this.history[index]
           if (pos) {
-            p5.vertex(pos.x, pos.y)
+            let opacity = p5.map(i, 0, this.historySize-1, minOpacity, maxOpacity)
+            let hue = p5.map(i, 0, this.historySize-1, 0, 255) // change hue from start to end of trail
+
+            this.p5.stroke(hue, 255, 255, opacity)
+            this.p5.strokeWeight(1)
+
+            if (i === this.historySize - 1) {
+              p5.beginShape()
+              p5.vertex(pos.x, pos.y)
+            } else {
+              let nextPos = this.history[(index + 1) % this.historySize]
+              if (nextPos) {
+                p5.vertex(nextPos.x, nextPos.y)
+              }
+            }
+
           }
         }
         this.p5.endShape()
@@ -91,13 +113,13 @@ const PerlinNoise = () => {
     }
     
     const particles = []
-
     let flowField = []
 
     p5.setup = () => {
       p5.createCanvas(p5.windowWidth, p5.windowHeight)
-      cols = p5.floor(p5.width / scl)
-      rows = p5.floor(p5.height / scl)
+      p5.colorMode(p5.HSB, 255)
+      cols = p5.floor(p5.width / scl) + 1
+      rows = p5.floor(p5.height / scl) + 1
       fr = p5.createP('')
 
       flowField = new Array(cols * rows)
@@ -105,7 +127,7 @@ const PerlinNoise = () => {
       for (let i = 0; i < 10000; i++) {
         particles.push(new Particle(p5))
       }
-      p5.background(51)
+      p5.background(10)
     }
 
     p5.draw = () => {
@@ -117,7 +139,7 @@ const PerlinNoise = () => {
           let angle = p5.noise(xoff, yoff, zoff) * p5.TWO_PI * 2
           let v = p5.createVector(p5.cos(angle), p5.sin(angle))
           flowField[index] = v
-          v.setMag(20)
+          v.setMag(1)
           xoff += inc
 
           // Uncomment to see the flow field vectors
@@ -143,7 +165,6 @@ const PerlinNoise = () => {
 
       fr.html(p5.floor(p5.frameRate()))
     }
-
     p5.windowResized = () => {
       p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
     }
@@ -154,7 +175,6 @@ const PerlinNoise = () => {
     return () => canvas.remove()
   }, [])
   
-
   return <div ref={canvasRef} id="perlin-noise" />
 }
 
