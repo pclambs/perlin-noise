1d320401682e947b6dc16d06148715cd396b2f4f
deepwubs <pchucklambs@gmail.com>
2023-11-03 22:31:57 -0500

    working flow field

--- src/components/PerlinNoise/index.jsx
+++ src/components/PerlinNoise/index.jsx
@@ -2,49 +2,45 @@ import React, { useEffect, useRef } from 'react'
 import p5 from 'p5'
 import './PerlinNoise.css'
 
-const PerlinNoise = () => {
-const canvasRef = useRef()
 
+const PerlinNoise = () => {
+  const canvasRef = useRef()
   const Sketch = p5 => {
-    const inc = 0.002
+    const inc = 0.005
+    const scl = 10
+    let cols, rows
+    let zoff = 0
+    let fr
 
     p5.setup = () => {
-      p5.createCanvas(p5.windowWidth, p5.windowHeight)
-      p5.pixelDensity(1)
+      p5.createCanvas(1000, 1000)
+      // (p5.windowWidth, p5.windowHeight)
+      cols = p5.floor(p5.width / scl)
+      rows = p5.floor(p5.height / scl)
+      fr = p5.createP('')
     }
 
     p5.draw = () => {
+      p5.background(225)
       let yoff = 0
-      
-      p5.loadPixels()
-      for (let y = 0; y < p5.height; y++) {
+      for (let y = 0; y < rows; y++) {
         let xoff = 0
-        for (let x = 0; x < p5.width; x++) {
-          let index = (x + y * p5.width) * 4
-          let r = p5.noise(xoff, yoff) * 255
-          p5.pixels[index + 0] = r
-          p5.pixels[index + 1] = r
-          p5.pixels[index + 2] = r
-          p5.pixels[index + 3] = 255
-
+        for (let x = 0; x < cols; x++) {
+          let angle = p5.noise(xoff, yoff, zoff) * p5.TWO_PI
+          let v = p5.createVector(p5.cos(angle), p5.sin(angle))
           xoff += inc
+          p5.stroke(0)
+          p5.push()
+          p5.translate(x * scl, y * scl)
+          p5.rotate(v.heading())
+          p5.line(0, 0, scl, 0)
+          p5.pop()
         }
         yoff += inc
+        zoff += 0.0001
       }
-      p5.noiseDetail(12, .5)
-      p5.updatePixels()
 
-      // start += inc
-
-      // p5.noLoop()
-
-      // let x = p5.map(p5.noise(xoff1), 0, 1, 0, p5.width)
-      // let y = p5.map(p5.noise(xoff2), 0, 1, 0, p5.height)
-
-      // xoff1 += 0.01
-      // xoff2 += 0.01
-
-      // p5.ellipse(x, y, 24, 24)
+      fr.html(p5.floor(p5.frameRate()))
     }
 
     p5.windowResized = () => {
